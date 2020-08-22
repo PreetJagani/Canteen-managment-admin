eb968c9bc43178bd141d227982cc307e01d9411b
PreetJagani <preetjagani@gmail.com>
2020-08-22 14:48:55 +0530

    library and menu added

---
+++ canteen_managment_library/src/main/java/com/canteenmanagment/canteen_managment_library/models/food.kt
@@ -0,0 +1,19 @@
+package com.canteenmanagment.canteen_managment_library.models
+
+class food(
+    var name : String?,
+    var price : Int?,
+    var imageUrls : List<String>,
+    var counterNumber : Int?,
+    var category : String?,
+    var available : Boolean
+) {
+
+    constructor() : this(null,null, emptyList<String>(),null,null,false)
+
+
+
+
+
+
+}
